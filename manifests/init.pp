75007be17ce314c0893345e755b52d85b22765a5
Simon Kurka <dev@simon.kurka.cc>
2016-02-24 20:23:43 +0100

    duplicate declaration error resolved

--- manifests/init.pp
+++ manifests/init.pp
@@ -2,11 +2,8 @@ class fastd (
   
 ) inherits fastd::params {
 
-  contain fastd::install
-  contain fastd::config
-
-  class { 'fastd::install': } ->
-  class { 'fastd::config': }
+  require fastd::install
+  require fastd::config
 
   create_resources('fastd::instance', hiera('fastd::instance', {}))
 
