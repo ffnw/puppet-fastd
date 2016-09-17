bf6a60e62c0c4c7d43c04aabd01a8b711af10021
Simon Kurka <dev@simon.kurka.cc>
2016-09-17 15:38:03 +0200

    Activating backports repo and workaround service file bug in bpo version

--- manifests/install.pp
+++ manifests/install.pp
@@ -1,24 +1,15 @@
 class fastd::install inherits fastd {
 
   include apt
+  include apt::backports
 
-  if(!defined(Package['apt-transport-https'])) {
-    package { 'apt-transport-https':
-      ensure => installed,
-    }
-  }
-
-  apt::source { 'universe-factory':
-    location => 'http://repo.universe-factory.net/debian',
-    release  => 'sid',
-    repos    => 'main',
-    key      => {
-      'id'     => '6664E7BDA6B669881EC52E7516EF3F64CB201D9C',
-      'server' => 'pgpkeys.mit.edu',
-    },
-  } ->
   package { 'fastd':
     ensure => 'installed',
+  } ->
+  file { '/lib/systemd/system/fastd@.service'
+    ensure => link,
+    target => '/lib/systemd/system/fastd.service',
+    replace => false
   }
 
   package { 'haveged':
