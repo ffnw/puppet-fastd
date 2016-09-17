58ba54ad990e3b58f8260acb653599b4fa2ee0a5
Simon Kurka <dev@simon.kurka.cc>
2016-09-17 15:42:08 +0200

    bugfix

--- manifests/install.pp
+++ manifests/install.pp
@@ -6,9 +6,9 @@ class fastd::install inherits fastd {
   package { 'fastd':
     ensure => 'installed',
   } ->
-  file { '/lib/systemd/system/fastd@.service'
-    ensure => link,
-    target => '/lib/systemd/system/fastd.service',
+  file { '/lib/systemd/system/fastd@.service':
+    ensure  => link,
+    target  => '/lib/systemd/system/fastd.service',
     replace => false
   }
 
