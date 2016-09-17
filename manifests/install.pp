972930d286165887f81fb876ae456f424839e1ab
Simon Kurka <dev@simon.kurka.cc>
2016-09-17 23:07:03 +0200

    pin to jessie-backports

--- manifests/install.pp
+++ manifests/install.pp
@@ -3,6 +3,12 @@ class fastd::install inherits fastd {
   include apt
   include apt::backports
 
+  apt::pin { 'fastd jessie-backports':
+    label    => 'jessie-backports',
+    priority => 500,
+    packages => [ 'fastd' ]
+  }
+
   package { 'fastd':
     ensure => 'installed',
   } ->
